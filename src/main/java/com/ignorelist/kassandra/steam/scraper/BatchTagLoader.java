061b172111dc6a6b5d365752d5aa4ac85de0b7fd
poison <rc.poison@gmail.com>
2016-03-03 22:05:39 +0100

    multithreaded batch tag loader

---
+++ src/main/java/com/ignorelist/kassandra/steam/scraper/BatchTagLoader.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ignorelist.kassandra.steam.scraper;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.SetMultimap;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author poison
+ */
+public class BatchTagLoader {
+
+	private final TagLoader tagLoader;
+	private final int nThreads;
+
+	public BatchTagLoader(TagLoader tagLoader, int nThreads) {
+		this.tagLoader=tagLoader;
+		this.nThreads=nThreads;
+	}
+
+	public BatchTagLoader(TagLoader tagLoader) {
+		this(tagLoader, Runtime.getRuntime().availableProcessors()+1);
+	}
+
+	public SetMultimap<Long, String> load(Iterable<Long> gameIds, final EnumSet<TagType> types) {
+		ExecutorService executorService=Executors.newFixedThreadPool(nThreads);
+		Map<Long, Future<Set<String>>> futureResults=new HashMap<>();
+		for (final Long gameId : gameIds) {
+			futureResults.put(gameId, executorService.submit(new Callable<Set<String>>() {
+				@Override
+				public Set<String> call() throws Exception {
+					return tagLoader.load(gameId, types);
+				}
+			}));
+		}
+		SetMultimap<Long, String> results=HashMultimap.create();
+		for (Map.Entry<Long, Future<Set<String>>> entry : futureResults.entrySet()) {
+			final Long key=entry.getKey();
+			final Future<Set<String>> value=entry.getValue();
+			try {
+				results.putAll(key, value.get());
+			} catch (InterruptedException ex) {
+				Logger.getLogger(BatchTagLoader.class.getName()).log(Level.SEVERE, null, ex);
+			} catch (ExecutionException ex) {
+				Logger.getLogger(BatchTagLoader.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+		return results;
+	}
+}
